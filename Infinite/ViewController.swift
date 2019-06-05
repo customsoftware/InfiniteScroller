82359d20a9d6633cf0562b956e16383c3d030795
Ken Cluff <kencluffutah@gmail.com>
2019-06-05 14:53:14 -0600

    Initial Commit

---
+++ Infinite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Infinite
+//
+//  Created by Kenneth Cluff on 6/5/19.
+//  Copyright © 2019 Kenneth Cluff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
